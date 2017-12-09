baba63ff0bc4eba06440e2ce97dc70127b9fbb50
Jan Fornoff <jan.fornoff@grandcentrix.net>
2017-12-09 15:51:43 +0100

    Day 9 Part 2

--- Day09/daynine/lib/daynine.ex
+++ Day09/daynine/lib/daynine.ex
@@ -12,60 +12,81 @@ defmodule Daynine do
       input_string
       |> String.trim()
       |> String.codepoints()
-      |> Enum.reduce(%{stack: Stack.empty(), score: 0, level: 0}, fn next_character, current ->
-           case Stack.peek(current.stack) do
-             nil ->
-               case next_character do
-                 "{" ->
-                   %{
-                     current
-                     | stack: Stack.push(current.stack, :open_group),
-                       level: current.level + 1
-                   }
-               end
-
-             :discard_next_token ->
-               %{current | stack: Stack.pop(current.stack)}
-
-             :open_garbage ->
-               case next_character do
-                 ">" -> %{current | stack: Stack.pop(current.stack)}
-                 "!" -> %{current | stack: Stack.push(current.stack, :discard_next_token)}
-                 _ -> current
-               end
-
-             :open_group ->
-               case next_character do
-                 "," ->
-                   current
-
-                 "!" ->
-                   %{current | stack: Stack.push(current.stack, :discard_next_token)}
-
-                 "<" ->
-                   %{current | stack: Stack.push(current.stack, :open_garbage)}
-
-                 "}" ->
-                   %{
-                     current
-                     | stack: Stack.pop(current.stack),
-                       score: current.score + current.level,
-                       level: current.level - 1
-                   }
-
-                 "{" ->
-                   case next_character do
-                     "{" ->
-                       %{
-                         current
-                         | stack: Stack.push(current.stack, :open_group),
-                           level: current.level + 1
-                       }
-                   end
-               end
-           end
-         end)
+      |> Enum.reduce(%{stack: Stack.empty(), score: 0, level: 0}, &consume/2)
 
     result.score
+  end
+
+  def consume(_next_character, %{stack: [:discard_next_token | _]} = current) do
+    %{current | stack: Stack.pop(current.stack)}
+  end
+
+  def consume(next_character, %{stack: [:open_group | _]} = current) do
+    case next_character do
+      "," ->
+        current
+
+      "!" ->
+        %{current | stack: Stack.push(current.stack, :discard_next_token)}
+
+      "<" ->
+        %{current | stack: Stack.push(current.stack, :open_garbage)}
+
+      "}" ->
+        %{
+          current
+          | stack: Stack.pop(current.stack),
+            score: current.score + current.level,
+            level: current.level - 1
+        }
+
+      "{" ->
+        %{
+          current
+          | stack: Stack.push(current.stack, :open_group),
+            level: current.level + 1
+        }
+    end
+  end
+
+  def consume(next_character, %{stack: [:open_garbage | _]} = current) do
+    case next_character do
+      ">" -> %{current | stack: Stack.pop(current.stack)}
+      "!" -> %{current | stack: Stack.push(current.stack, :discard_next_token)}
+      _ -> current
+    end
+  end
+
+  def consume("{", %{stack: _, score: _, level: _} = current) do
+    %{current | stack: Stack.push(current.stack, :open_group), level: current.level + 1}
+  end
+
+  def solve_second_part(input_string \\ File.read!("#{__DIR__}/input.txt")) do
+    input_string
+    |> String.trim()
+    |> String.split("")
+    |> count_garbage(false, 0)
+  end
+
+  def count_garbage([], _, count), do: count
+
+  def count_garbage(["!", _ | rest], in_garbage, count) do
+    count_garbage(rest, in_garbage, count)
+  end
+
+  def count_garbage(["<" | rest], false, count) do
+    count_garbage(rest, true, count)
+  end
+
+  def count_garbage([_ | rest], false, count) do
+    count_garbage(rest, false, count)
+  end
+
+  def count_garbage([">" | rest], true, count) do
+    count_garbage(rest, false, count)
+  end
+
+  def count_garbage([_ | rest], true, count) do
+    count_garbage(rest, true, count + 1)
   end
 end
--- Day09/daynine/test/daynine_test.exs
+++ Day09/daynine/test/daynine_test.exs
@@ -38,4 +38,34 @@ defmodule DaynineTest do
       assert(Daynine.solve_first_part("{{<a!>},{<a!>},{<a!>},{<ab>}}") == 3)
     end
   end
+
+  describe ".solve_second_part" do
+    test "0 characters" do
+      assert Daynine.solve_second_part("<>") == 0
+    end
+
+    test "17 characters" do
+      assert Daynine.solve_second_part("<random characters>") == 17
+    end
+
+    test "3 characters" do
+      assert Daynine.solve_second_part("<<<<>") == 3
+    end
+
+    test "2 characters" do
+      assert Daynine.solve_second_part("<{!>}>") == 2
+    end
+
+    test "0 characters with negation" do
+      assert Daynine.solve_second_part("<!!>") == 0
+    end
+
+    test "0 characters with negations (second)" do
+      assert Daynine.solve_second_part("<!!!>>") == 0
+    end
+
+    test "10 characters" do
+      assert Daynine.solve_second_part("<{o\"i!a,<{i<a>") == 10
+    end
+  end
 end
