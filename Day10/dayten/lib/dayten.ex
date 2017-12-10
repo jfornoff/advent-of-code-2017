612a004853e67bcbbfeb97a203968c3dbdc0cd75
Jan Fornoff <jan.fornoff@grandcentrix.net>
2017-12-10 11:35:56 +0100

    Day 10 Part 2

--- Day10/dayten/lib/dayten.ex
+++ Day10/dayten/lib/dayten.ex
@@ -1,4 +1,5 @@
 defmodule Dayten do
+  import Bitwise
   @lengths_input "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70"
   @list 0..255 |> Enum.into([])
 
@@ -19,22 +20,27 @@ defmodule Dayten do
       lengths
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
-      |> Enum.reduce(Iteration.initial(list), fn swap_length, iteration ->
-           new_list =
-             iteration.current_list
-             |> cycle_by(iteration.current_index)
-             |> Enum.reverse_slice(0, swap_length)
-             |> cycle_by(length(iteration.current_list) - iteration.current_index)
-
-           %Iteration{
-             current_list: new_list,
-             current_index: next_focused_index(iteration, swap_length),
-             skip_length: iteration.skip_length + 1
-           }
-         end)
+      |> run_round(list)
 
     [first_item, second_item | _] = result.current_list
     first_item * second_item
+  end
+
+  defp run_round(lengths, input) do
+    lengths
+    |> Enum.reduce(Iteration.initial(input), fn swap_length, iteration ->
+         new_list =
+           iteration.current_list
+           |> cycle_by(iteration.current_index)
+           |> Enum.reverse_slice(0, swap_length)
+           |> cycle_by(length(iteration.current_list) - iteration.current_index)
+
+         %Iteration{
+           current_list: new_list,
+           current_index: next_focused_index(iteration, swap_length),
+           skip_length: iteration.skip_length + 1
+         }
+       end)
   end
 
   defp next_focused_index(iteration, swap_length) do
@@ -52,5 +58,50 @@ defmodule Dayten do
 
   defp swap_and_concat({first, last}) do
     last ++ first
+  end
+
+  def solve_second_part(lengths \\ @lengths_input) do
+    lengths
+    |> to_bytes
+    |> append_predefined_lengths
+    |> compute_sparse_hash(0..255 |> Enum.into([]))
+    |> compute_dense_hash
+    |> format_hexadecimal
+  end
+
+  defp to_bytes(lengths) do
+    to_charlist(lengths)
+  end
+
+  defp append_predefined_lengths(lengths) do
+    lengths ++ [17, 31, 73, 47, 23]
+  end
+
+  defp compute_sparse_hash(lengths, start_list) do
+    result =
+      lengths
+      |> Stream.cycle()
+      |> Enum.take(length(lengths) * 64)
+      |> run_round(start_list)
+
+    result.current_list
+  end
+
+  defp compute_dense_hash(sparse_hash) do
+    sparse_hash
+    |> Enum.chunk_every(16)
+    |> Enum.map(&xor_elements/1)
+  end
+
+  defp xor_elements(elements) do
+    Enum.reduce(elements, 0, &(&1 ^^^ &2))
+  end
+
+  defp format_hexadecimal(elements) do
+    elements
+    |> Enum.map(&Integer.to_string(&1, 16))
+    |> Enum.map(&String.pad_leading(&1, 2, "0"))
+    |> Enum.map(&String.downcase/1)
+    |> Enum.join()
   end
 end
--- Day10/dayten/test/dayten_test.exs
+++ Day10/dayten/test/dayten_test.exs
@@ -22,4 +22,22 @@ defmodule DaytenTest do
       assert Dayten.solve_first_part("3,4,1,5", [0, 1, 2, 3, 4]) == 12
     end
   end
+
+  describe ".solve_second_part" do
+    test "empty string" do
+      assert Dayten.solve_second_part("") == "a2582a3a0e66e6e86e3812dcb672a272"
+    end
+
+    test "AoC 17" do
+      assert Dayten.solve_second_part("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd"
+    end
+
+    test "1,2,3" do
+      assert Dayten.solve_second_part("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d"
+    end
+
+    test "1,2,4" do
+      assert Dayten.solve_second_part("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e"
+    end
+  end
 end
